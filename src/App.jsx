2021995634c0de497af3ef864ea25cca7f77ec10
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-02 17:38:48 +0100

    Few updates added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Admin from "./Components/AdminDashBoard/Admin";
 
 export default function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Admin />
     </>
   );
 }
