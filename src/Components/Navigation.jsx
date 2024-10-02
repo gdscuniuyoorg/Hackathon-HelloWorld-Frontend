97dcee4fa222fa5cf15d767c4e6ef631feaf52ee
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-02 17:39:13 +0100

    Admin page created

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,18 @@
+export default function Navigation() {
+  return (
+    <nav>
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <div className="desktopNav">
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+        <button className="cta">Take Attendance</button>
+      </div>
+    </nav>
+  );
+}
