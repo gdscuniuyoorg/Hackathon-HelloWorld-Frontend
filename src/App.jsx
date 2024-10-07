449f0da256debd42471a0aa27c78ae9be46131f4
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-07 13:26:34 +0100

    There is alot to be done.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import Admin from "./Components/AdminDashBoard/Admin";
-
+// import Student from "./Components/StudentDashBoard/Student";
 export default function App() {
   return (
     <>
       <Admin />
+      {/* <Student /> */}
     </>
   );
 }
