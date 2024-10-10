3e9a64d14890829b55f9a19b9237b8da603afb8d
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-10 19:05:59 +0100

    login page designed. API in process

--- src/Components/LandingPage/main/Login.jsx
+++ src/Components/LandingPage/main/Login.jsx
@@ -1,7 +1,56 @@
+import styles from "./form.module.css";
+import twitter from "../assets/twitter.png";
+import google from "../assets/Google.png";
 export default function Login() {
   return (
     <>
-      <form action="#"></form>
+      <section className={styles.alt}>
+        <h2>Login</h2>
+        <div>
+          <div className={`d_flex ${styles.altLogin}`}>
+            <img src={google} alt="google" /> Continue With Google
+          </div>
+          <div className={`d_flex ${styles.altLogin}`}>
+            <img src={twitter} alt="google" /> Continue With Twitter
+          </div>
+        </div>
+        <div className={`d_flex ${styles.or}`}>
+          <hr />
+          OR
+          <hr />
+        </div>
+      </section>
+      <form action="#" className={styles.form}>
+        <div>
+          <label htmlFor="studentID" className="d_flex">
+            <span>Student ID</span>
+            <input
+              type="text"
+              name="studentID"
+              id="studentID"
+              placeholder="Enter your ID"
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="studentID" className="d_flex">
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </label>
+          <button type="button">forget password?</button>
+        </div>
+        <button type="submit" className={styles.submit}>
+          Login
+        </button>
+        <p>
+          Don&apos;t have account? <span>Sign Up</span>
+        </p>
+      </form>
     </>
   );
 }
--- src/Components/LandingPage/main/SignUp.jsx
+++ src/Components/LandingPage/main/SignUp.jsx
@@ -1,7 +1,41 @@
+import styles from "./form.module.css";
 export default function SignUp() {
   return (
     <>
-      <form action="#"></form>
+      <section className={styles.alt}>
+        <h2>Sign Up</h2>
+      </section>
+      <form action="#" className={styles.form}>
+        <div>
+          <label htmlFor="studentID" className="d_flex">
+            <span>Student ID</span>
+            <input
+              type="text"
+              name="studentID"
+              id="studentID"
+              placeholder="Enter your ID"
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="studentID" className="d_flex">
+            <span>Password</span>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </label>
+          <button type="button">forget password?</button>
+        </div>
+        <button type="submit" className={styles.submit}>
+          Login
+        </button>
+        <p>
+          Don&apos;t have account? <span>Sign Up</span>
+        </p>
+      </form>
     </>
   );
 }
