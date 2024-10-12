5695efb5e00ed8c2894062cb2a31984516fa7f99
SpiDher <penivera655@gmail.com>
2024-10-12 07:35:05 +0100

    I just dey warn you

--- src/Components/LandingPage/components/LandingPageBenefits.jsx
+++ src/Components/LandingPage/components/LandingPageBenefits.jsx
@@ -13,36 +13,30 @@ export default function LandingPageBenefits() {
       </header>
       <section>
         <div>
-          <img src="" alt="illustration" />
           <h4>Benefit One</h4>
           <p>Smart and effective attendace tracking system</p>
         </div>
 
         <div>
-          <img src="" alt="illustration" />
           <h4>Benefit One</h4>
           <p>Accurate data: Reliable and consistent data</p>
         </div>
 
         <div>
-          <img src="" alt="illustration" />
           <h4>Benefit Two</h4>
           <p>Convenience: Allows students to clock in easily using their mobile devices</p>
         </div>
 
         <div>
-          <img src="" alt="illustration" />
           <h4>Support</h4>
           <p>Support our ministry, let's make life easy for you</p>
         </div>
 
         <div>
-          <img src="" alt="illustration" />
           <h4>Notice</h4>
           <p>This is still in Beta mode so you might experience glitches</p>
         </div>
         <div>
-          <img src="" alt="illustration" />
           <h4>Requirements</h4>
           <p>PLEAE USE CHROMIUM BASED BROWSERS, MOST PREFERABLY CHROME</p>
         </div>
